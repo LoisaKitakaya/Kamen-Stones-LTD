e16d240218f4ab9f14cf84d4533baa3b3517478c
Freedom Loisa <kitakayaloisa@gmail.com>
2022-04-28 15:28:39 +0300

    finished career page

--- src/views/CareerView.vue
+++ src/views/CareerView.vue
@@ -1,4 +1,362 @@
-<template></template>
+<template>
+  <div class="career-container container-fluid">
+    <hr />
+    <h1 class="text-center">Work With Us</h1>
+    <hr />
+    <div class="jobs">
+      <p class="text-center intro-to-jobs">
+        Open opportunities at Kamen Stones LTD
+      </p>
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              Human Resource Manager
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              <h4>Job Details</h4>
+              <br />
+              <h5>Salary</h5>
+              <p>$130,000 - $150,000 a year</p>
+              <br />
+              <h5>Job type</h5>
+              <p>Full time</p>
+              <br />
+              <h5>Full Job Description</h5>
+              <p>
+                We are Psychology Today – the world's #1 psychology site visited
+                by over 40 million people a month. We match thousands of
+                therapists with clients every day. Think Airbnb, but for
+                Therapy.
+              </p>
+              <p>
+                Join us on our mission to bring mental health and therapy to the
+                world.
+              </p>
+              <p>
+                Ideal candidates have 4+ years of relevant experience. Expertise
+                in Go, AWS and Vue.js or React is a plus. Hours are Eastern
+                Standard Time; the position is remote.
+              </p>
+              <p>We offer:</p>
+              <ul>
+                <li>Work/life balance.</li>
+                <li>
+                  A product and team that will inspire you - so you can do
+                  meaningful work with people who make you laugh!
+                </li>
+                <li>
+                  A healthy, profitable and stable company where team members
+                  stay for many years.
+                </li>
+                <li>
+                  The freedom to work remotely (most of us are in the US,
+                  Canada, or South America - as we all work on US Eastern time).
+                </li>
+                <li>
+                  A supportive platform and a flat structure to do innovative
+                  work, be acknowledged, and make a difference.
+                </li>
+                <li>Annual compensation from: $130 - $150K + Bonus</li>
+              </ul>
+              <br />
+              <p>Requirements:</p>
+              <ul>
+                <li>
+                  Ability to work independently without supervision to meet
+                  deadlines
+                </li>
+                <li>
+                  Well organized, results oriented self-starter with a sense of
+                  urgency
+                </li>
+                <li>
+                  Exceptionally strong technical, analytical, written, and
+                  verbal communication skills
+                </li>
+                <li>
+                  Ability to work with continuous integration and continuous
+                  deployment
+                </li>
+                <li>
+                  Experience with infrastructure as code using tools like
+                  Terraform or CloudFormation
+                </li>
+                <li>
+                  Ability to work in and adapt to multiple backend languages
+                  with a focus on GoLang
+                </li>
+                <li>
+                  Must have experience working with AWS managed services and
+                  related tooling
+                </li>
+                <li>
+                  Good understanding of and experience implementing server side
+                  rendering techniques
+                </li>
+                <li>
+                  Expertise with writing frontend applications in VueJS or React
+                </li>
+              </ul>
+              <br />
+              <p>Benefits:</p>
+              <ul>
+                <li>Flexible schedule</li>
+                <li>Paid time off</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingTwo">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+              Security Personnel
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              <h4>Job Details</h4>
+              <br />
+              <h5>Salary</h5>
+              <p>$130,000 - $150,000 a year</p>
+              <br />
+              <h5>Job type</h5>
+              <p>Full time</p>
+              <br />
+              <h5>Full Job Description</h5>
+              <p>
+                We are Psychology Today – the world's #1 psychology site visited
+                by over 40 million people a month. We match thousands of
+                therapists with clients every day. Think Airbnb, but for
+                Therapy.
+              </p>
+              <p>
+                Join us on our mission to bring mental health and therapy to the
+                world.
+              </p>
+              <p>
+                Ideal candidates have 4+ years of relevant experience. Expertise
+                in Go, AWS and Vue.js or React is a plus. Hours are Eastern
+                Standard Time; the position is remote.
+              </p>
+              <p>We offer:</p>
+              <ul>
+                <li>Work/life balance.</li>
+                <li>
+                  A product and team that will inspire you - so you can do
+                  meaningful work with people who make you laugh!
+                </li>
+                <li>
+                  A healthy, profitable and stable company where team members
+                  stay for many years.
+                </li>
+                <li>
+                  The freedom to work remotely (most of us are in the US,
+                  Canada, or South America - as we all work on US Eastern time).
+                </li>
+                <li>
+                  A supportive platform and a flat structure to do innovative
+                  work, be acknowledged, and make a difference.
+                </li>
+                <li>Annual compensation from: $130 - $150K + Bonus</li>
+              </ul>
+              <br />
+              <p>Requirements:</p>
+              <ul>
+                <li>
+                  Ability to work independently without supervision to meet
+                  deadlines
+                </li>
+                <li>
+                  Well organized, results oriented self-starter with a sense of
+                  urgency
+                </li>
+                <li>
+                  Exceptionally strong technical, analytical, written, and
+                  verbal communication skills
+                </li>
+                <li>
+                  Ability to work with continuous integration and continuous
+                  deployment
+                </li>
+                <li>
+                  Experience with infrastructure as code using tools like
+                  Terraform or CloudFormation
+                </li>
+                <li>
+                  Ability to work in and adapt to multiple backend languages
+                  with a focus on GoLang
+                </li>
+                <li>
+                  Must have experience working with AWS managed services and
+                  related tooling
+                </li>
+                <li>
+                  Good understanding of and experience implementing server side
+                  rendering techniques
+                </li>
+                <li>
+                  Expertise with writing frontend applications in VueJS or React
+                </li>
+              </ul>
+              <br />
+              <p>Benefits:</p>
+              <ul>
+                <li>Flexible schedule</li>
+                <li>Paid time off</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingThree">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              Senior Software Engineer
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingThree"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              <h4>Job Details</h4>
+              <br />
+              <h5>Salary</h5>
+              <p>$130,000 - $150,000 a year</p>
+              <br />
+              <h5>Job type</h5>
+              <p>Full time</p>
+              <br />
+              <h5>Full Job Description</h5>
+              <p>
+                We are Psychology Today – the world's #1 psychology site visited
+                by over 40 million people a month. We match thousands of
+                therapists with clients every day. Think Airbnb, but for
+                Therapy.
+              </p>
+              <p>
+                Join us on our mission to bring mental health and therapy to the
+                world.
+              </p>
+              <p>
+                Ideal candidates have 4+ years of relevant experience. Expertise
+                in Go, AWS and Vue.js or React is a plus. Hours are Eastern
+                Standard Time; the position is remote.
+              </p>
+              <p>We offer:</p>
+              <ul>
+                <li>Work/life balance.</li>
+                <li>
+                  A product and team that will inspire you - so you can do
+                  meaningful work with people who make you laugh!
+                </li>
+                <li>
+                  A healthy, profitable and stable company where team members
+                  stay for many years.
+                </li>
+                <li>
+                  The freedom to work remotely (most of us are in the US,
+                  Canada, or South America - as we all work on US Eastern time).
+                </li>
+                <li>
+                  A supportive platform and a flat structure to do innovative
+                  work, be acknowledged, and make a difference.
+                </li>
+                <li>Annual compensation from: $130 - $150K + Bonus</li>
+              </ul>
+              <br />
+              <p>Requirements:</p>
+              <ul>
+                <li>
+                  Ability to work independently without supervision to meet
+                  deadlines
+                </li>
+                <li>
+                  Well organized, results oriented self-starter with a sense of
+                  urgency
+                </li>
+                <li>
+                  Exceptionally strong technical, analytical, written, and
+                  verbal communication skills
+                </li>
+                <li>
+                  Ability to work with continuous integration and continuous
+                  deployment
+                </li>
+                <li>
+                  Experience with infrastructure as code using tools like
+                  Terraform or CloudFormation
+                </li>
+                <li>
+                  Ability to work in and adapt to multiple backend languages
+                  with a focus on GoLang
+                </li>
+                <li>
+                  Must have experience working with AWS managed services and
+                  related tooling
+                </li>
+                <li>
+                  Good understanding of and experience implementing server side
+                  rendering techniques
+                </li>
+                <li>
+                  Expertise with writing frontend applications in VueJS or React
+                </li>
+              </ul>
+              <br />
+              <p>Benefits:</p>
+              <ul>
+                <li>Flexible schedule</li>
+                <li>Paid time off</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <p class="text-center">
+      <router-link to="/contacts">Contact</router-link> us to clarify anything
+      related to our job postings and opportunities.
+    </p>
+    <hr />
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,4 +364,29 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.career-container {
+  padding: 2rem;
+}
+
+.intro-to-jobs {
+  text-decoration: underline;
+}
+
+.accordion-body {
+  width: 70%;
+  margin: 1rem auto;
+  border-left: 1px solid rgb(190, 190, 190);
+  border-right: 1px solid rgb(190, 190, 190);
+}
+
+@media screen and (max-width: 600px) {
+  .career-container {
+    padding: 1rem !important;
+  }
+
+  .accordion-body {
+    width: auto !important;
+  }
+}
+</style>
